8a416caf05bfc160d373d22a4d373721faa0d412
vkumarsinghnoida <84572128+vkumarsinghnoida@users.noreply.github.com>
2023-02-12 09:12:24 +0530

    Update deepstream_pipeline.py

--- Deepstream pipeline class/deepstream_pipeline.py
+++ Deepstream pipeline class/deepstream_pipeline.py
@@ -47,16 +47,36 @@ class ObjectDetection:
 
     def start(self):
         # Use the osd_sink_pad_buffer_probe function and the pyds library to extract the metadata of the object detections
-        def osd_sink_pad_buffer_probe(pad, info, u_data):
-            sample = info.get_buffer()
-            if sample:
-                # Get the object detection metadata from the sample using the pyds library
-                obj_meta = pyds.NvDsObjectMeta.cast(sample.get_nvds_meta(0))
-                while obj_meta:
-                    # Display the detections
-                    # Code to display the detections goes here
-                    obj_meta = obj_meta.next
-            return Gst.PadProbeReturn.OK
+        def osd_sink_pad_buffer_probe(pad,info,u_data):
+    
+            gst_buffer = info.get_buffer()
+            batch_meta = pyds.gst_buffer_get_nvds_batch_meta(hash(gst_buffer))
+            l_frame = batch_meta.frame_meta_list
+            while l_frame is not None:
+                try:
+                    frame_meta = pyds.NvDsFrameMeta.cast(l_frame.data)
+                except StopIteration:
+                    break
+
+                while l_obj is not None:
+                    try:
+                        obj_meta=pyds.NvDsObjectMeta.cast(l_obj.data)
+                    except StopIteration:
+                        break
+                    obj_counter[obj_meta.class_id] += 1
+                    obj_meta.rect_params.border_color.set(0.0, 0.0, 1.0, 0.0)
+                    try: 
+                        l_obj=l_obj.next
+                    except StopIteration:
+                        break
+
+             # Acquiring a display meta object. The memory ownership remains in
+             try:
+                 l_frame=l_frame.next
+             except StopIteration:
+                 break
+			
+        return Gst.PadProbeReturn.OK
 
         osd_sink_pad = self.osd.get_static_pad("sink")
         osd_sink_pad.add_probe(Gst.PadProbeType.BUFFER, osd_sink_pad_buffer_probe, None)
