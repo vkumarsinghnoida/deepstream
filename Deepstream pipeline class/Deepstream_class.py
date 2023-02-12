1f46739d9e44d27cbb7075d357f4dbd07060cfaf
vkumarsinghnoida <84572128+vkumarsinghnoida@users.noreply.github.com>
2023-02-12 09:50:54 +0530

    Update Deepstream_class.py

--- Deepstream pipeline class/Deepstream_class.py
+++ Deepstream pipeline class/Deepstream_class.py
@@ -13,4 +13,18 @@ class ObjectDetection:
         self.config_file_path = config_file_path
 
         Gst.init(None)
+        self.pipeline = Gst.pipeline()
+        
+        self.source = Gst.ElementFactory.make("filesrc", "file-source")
+        self.h264parser = Gst.ElementFactory.make("h264parse", "h264-parser")
+        self.decoder = Gst.ElementFactory.make("nvv4l2decoder", "nvv4l2-decoder")
+        self.streammux = Gst.ElementFactory.make("nvstreammux", "Stream-muxer")
+        self.pgie = Gst.ElementFactory.make("nvinfer", "primary-inference")
+        self.nvvidconv = Gst.ElementFactory.make("nvvideoconvert", "convertor")
+        self.nvosd = Gst.ElementFactory.make("nvdsosd", "onscreendisplay")
+        self.sink = Gst.ElementFactory.make("nveglglessink", "nvvideo-renderer")
+        self.sinkpad = self.streammux.get_request_pad("sink_0")
+        self.srcpad = self.decoder.get_static_pad("src")
+        
+        
 
