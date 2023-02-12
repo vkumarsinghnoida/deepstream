156a3c520182164a22fbcbcb1bef335eafa84563
vkumarsinghnoida <84572128+vkumarsinghnoida@users.noreply.github.com>
2023-02-12 09:43:37 +0530

    Create Deepstream_class.py

---
+++ Deepstream pipeline class/Deepstream_class.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+import gi
+import sys
+sys.path.append(../)
+gi.require_version('Gst', '1.0')
+from gi.repository import Gst, Glib
+import pyds
+
+class ObjectDetection:
+    def __init__(self, stream_url, config_file_path):
+        self.stream_url = stream_url
+        self.config_file_path = config_file_path
