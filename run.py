0475472a5c98e5a38452992a211e37cd86bd072d
vkumarsinghnoida <84572128+vkumarsinghnoida@users.noreply.github.com>
2023-02-21 15:09:30 +0530

    Update run.py

--- run.py
+++ run.py
@@ -8,6 +8,40 @@ from gi.repository import GLib, Gst
 from deepstream_class import Pipeline 
 import pyds
 
+def osd_sink_pad_buffer_probe(pad,info,u_data):
+    
+
+    gst_buffer = info.get_buffer()
+    batch_meta = pyds.gst_buffer_get_nvds_batch_meta(hash(gst_buffer))
+    l_frame = batch_meta.frame_meta_list
+    while l_frame is not None:
+        try:
+            frame_meta = pyds.NvDsFrameMeta.cast(l_frame.data)
+        except StopIteration:
+            break
+
+        frame_number=frame_meta.frame_num
+        num_rects = frame_meta.num_obj_meta
+        l_obj=frame_meta.obj_meta_list
+        while l_obj is not None:
+            try:
+                obj_meta=pyds.NvDsObjectMeta.cast(l_obj.data)
+            except StopIteration:
+                break
+            obj_counter[obj_meta.class_id] += 1
+            obj_meta.rect_params.border_color.set(0.0, 0.0, 1.0, 0.0)
+            try: 
+                l_obj=l_obj.next
+            except StopIteration:
+                break
+
+        try:
+            l_frame=l_frame.next
+        except StopIteration:
+            break
+			
+    return Gst.PadProbeReturn.OK
+
 def main(args):
     pipeline = Pipeline(args[1], args[2])
     pipeline.osdsinkpad.add_probe(Gst.PadProbeType.BUFFER, osd_sink_pad_buffer_probe, 0)
